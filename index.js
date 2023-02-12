cdf7c71568fe22973c699304fa75d367d401f867
mullinsmmm <matt.mullins@live.com>
2023-02-12 14:36:41 +0000

    1 file modified

--- index.js
+++ index.js
@@ -7,43 +7,10 @@ const settings = {
 
 //sketch function
 const sketch = () => {
-  let x, y, w, h;
-
   //render Function
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
-
-    //Declare Parameters
-    //0.5 = Half canvas
-    //0.1 = 1/10th canvas
-    //0.6 = 6/10th canvas
-    x = width * 0.5;
-    y = height * 0.5;
-    w = width * 0.6;
-    h = height * 0.1;
-
-    //Drawing rectangle code block
-    //save to avoid disrupting code later down
-    //Translate points to make rect central
-    context.save();
-    context.translate(x, y);
-    context.translate(w * -0.5, h * -0.5);
-
-    context.strokeStyle = 'red';
-
-    //acturallly draw rect, from top left, along to top right then followed
-    //finally adding a stroke
-    context.beginPath();
-    context.moveTo(0, 0);
-    context.lineTo(w, 0);
-    context.lineTo(w, h);
-    context.lineTo(0, h);
-    context.closePath();
-    context.stroke();
-
-    //Restore x,y,w,h to initial declared value
-    context.restore();
   };
 };
 
