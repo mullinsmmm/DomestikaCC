8b10260a447c6b67a92b7f603d8ff6d77f02c34d
mullinsmmm <matt.mullins@live.com>
2023-02-13 11:43:43 +0000

    FullSketch with comments

--- index.js
+++ index.js
@@ -1,161 +1,190 @@
-//API ELEMENTS USED - must install dependencies to use
-//called to use canvas sketch
+//API ELEMENTS USED
+//Calls canvas-sketch
 const canvasSketch = require('canvas-sketch');
-//called to use Math
+//Calls math
 const math = require('canvas-sketch-util/math');
-//Called to use random function
+//Calls random function
 const random = require('canvas-sketch-util/random');
+//Calls color
+const Color = require('canvas-sketch-util/color');
+//Calls Riso color palette
+const risoColors = require('riso-colors');
+
+//Adds a random seed, dictated by the date and time
+// const seed = '599404';
+const seed = Date.now();
 
 //SETUP FUNCTION
 const settings = {
   dimensions: [1080, 1080],
-  //animate: true,
+  name: seed,
 };
 
 //SKETCH FUNCTION
-const sketch = () => {
+const sketch = ({ context, width, height }) => {
+  random.setSeed(seed);
+
   //Variable Decleration
   let x, y, w, h, fill, stroke, blend;
-  let angle, rx, ry;
 
-  //number of rects
+  //Number of Rects
   const num = 40;
-  //const degrees = -30;
+  //Angle of skew
+  const degrees = -30;
 
-  //const rects = [];
+  const rects = [];
 
-  //const tectColors = [
-  //random.pick(risoColors),
-  //random.pick(risoColors),
-  //random.pick(risoColors),
-  //];
+  const rectColors = [random.pick(risoColors), random.pick(risoColors)];
 
-  //const bgColor = random.pick(risoColors).hex;
+  const bgColor = random.pick(risoColors).hex;
 
-  // for (let i = 0; i < num; i++) {
-  //   x = random.range(0, width);
-  //   y = random.range(0, height);
-  //   w = random.ramge(600, width);
-  //   h = random.range(40, 200);
+  const mask = {
+    radius: width * 0.4,
+    sides: 3,
+    x: width * 0.5,
+    y: height * 0.58,
+  };
 
-  //   fill = random.pick(rectColors).hex;
-  //   stroke = random.pick(rectColors).hex;
+  //Variables outside of RENDER FUNCTION to enable save of specific frame (Not Applicable in browser)
+  for (let i = 0; i < num; i++) {
+    x = random.range(0, width);
+    y = random.range(0, height);
+    w = random.range(600, width);
+    h = random.range(40, 200);
 
-  //   blend = random.value() > 0.5 ? 'overlay' : 'source-over';
+    fill = random.pick(rectColors).hex;
+    stroke = random.pick(rectColors).hex;
 
-  //   rects.push({ x, y, w, h, fill, stroke, blend });
-  // }
+    blend = random.value() > 0.5 ? 'overlay' : 'source-over';
+
+    rects.push({ x, y, w, h, fill, stroke, blend });
+  }
 
   //RENDER FUNCTION
   return ({ context, width, height }) => {
-    // context.fillStyle = bgColor;
-
-    //Background colour
-    context.fillStyle = 'pink';
+    //Background color
+    context.fillStyle = bgColor;
     //Background fill area
     context.fillRect(0, 0, width, height);
 
-    // rects.forEach((rect) => {
-    //   const { x, y, w, h, fill, stroke, blend } = rect;
-    //   let shadowColor;
+    //Good practice to save then restore variables when altering them
+    context.save();
+    //Move composition to centee of canvas
+    context.translate(mask.x, mask.y);
 
-    //   context.save();
+    //Draws Polygon clipping mask
+    drawPolygon({ context, radius: mask.radius, sides: mask.sides });
 
-    //   context.strokestyle = stroke;
-    //   context.fillstyle = fill;
-    //   context.linewidth = 10;
+    context.clip();
 
-    //   context.globalCompositeOperation = 'blend';
+    //forEach instead of forLoop
+    rects.forEach((rect) => {
+      //Variable Decleration
+      const { x, y, w, h, fill, stroke, blend } = rect;
+      let shadowColor;
 
-    //   drawSkewedRect({ context, w, h, degrees });
-
-    //   shadowColor = Color.offsetHSL(fill, 0, 0, -20);
-    //   shadowColor.rgba[3] = 0.5;
-
-    //   context.shadowColor = color.style(shadowColor.rgba);
-    //   context.shadowOffsetX = -10;
-    //   context.shadowOffsetY = 20;
-
-    //   context.fill();
-
-    //   context.shadowColor = null;
-    //   context.stroke();
-
-    //   context.globalCompositeOperation = 'source-over';
-
-    //   context.lineWidth = 2;
-    //   contextStrokeStyle = 'black';
-    //   context.stroke();
-
-    //   context.restore();
-    // });
-
-    //for loop encompassing drawSkewedRect function and parameters.
-    //if i is less that the value of num (above) call drawSkewedRect one more time and repeat
-    for (let i = 0; i < num; i++) {
-      //Declare Parameters
-      //0.5 = Half canvas
-      //0.1 = 1/10th canvas
-      //0.6 = 6/10th canvas
-      x = random.range(0, width);
-      y = height * 0.5;
-      w = width * 0.6;
-      h = height * 0.1;
-
-      //Good practice to save then restore after code block when using translate so x,y,w,h can be used later in code if needed
+      //Good Practice
       context.save();
-
-      //Moves rect to centre of canvas
+      //Centres Mask
+      context.translate(-mask.x, -mask.y);
+      //Centres x Y values
       context.translate(x, y);
-      //line colour
-      context.strokeStyle = 'blue';
 
-      //context.strokeRect(w * -0.5, h * -0.5, w, h);
+      //Stroke controls
+      context.strokeStyle = stroke;
+      context.fillStyle = fill;
+      context.lineWidth = 10;
 
-      //Call draw rect function (within the context of the canvas)
-      drawSkewedRect({ context });
+      //Blend Mode
+      context.globalCompositeOperation = blend;
 
+      //Calls draw rect function from below
+      drawSkewedRect({ context, w, h, degrees });
+
+      //Shadow color control
+      shadowColor = Color.offsetHSL(fill, 0, 0, -20);
+      shadowColor.rgba[3] = 0.5;
+
+      //Offset of colors
+      context.shadowColor = Color.style(shadowColor.rgba);
+      context.shadowOffsetX = -10;
+      context.shadowOffsetY = 20;
+
+      context.fill();
+
+      context.shadowColor = null;
+      context.stroke();
+
+      context.globalCompositeOperation = 'source-over';
+
+      context.lineWidth = 2;
+      context.strokeStyle = 'black';
       context.stroke();
 
       context.restore();
-    }
+    });
+
+    context.restore();
+
+    // polygon outline
+    context.save();
+    context.translate(mask.x, mask.y);
+    context.lineWidth = 20;
+
+    drawPolygon({
+      context,
+      radius: mask.radius - context.lineWidth,
+      sides: mask.sides,
+    });
+
+    context.globalCompositeOperation = 'color-burn';
+    context.strokeStyle = rectColors[0].hex;
+    context.stroke();
+
+    context.restore();
   };
 };
 
 //FUNCTION DECLERATION
-//Creates Skewed box function context = draw area,w = width, h = height, degrees = angle of skew
+//Creates skewed box function context=draw area, w=width h=height degrees=angle of skew
 const drawSkewedRect = ({ context, w = 600, h = 200, degrees = -45 }) => {
-  //Variable Decleration (uses const as its inside a function and doesnt change)
-  //used 'util' cavnas-sketch API to convert angle to radiun
-  //declare radius, horizonal = 0, vertical = 90
+  //Converts degrees to radians using math API
   const angle = math.degToRad(degrees);
-  //radius * width
+  //works out radians * width
   const rx = Math.cos(angle) * w;
   const ry = Math.sin(angle) * w;
 
-  //good practice to save then restore
+  //Good practice
   context.save();
-
   //translates points so when drawn point by point can be represented as 0
   context.translate(rx * -0.5, (ry + h) * -0.5);
 
-  //Draw rect point by point isuing rx/ry to decide angle
+  //Draw rect point by point using rx/ry to decide angle
   context.beginPath();
   context.moveTo(0, 0);
   context.lineTo(rx, ry);
   context.lineTo(rx, ry + h);
   context.lineTo(0, h);
   context.closePath();
-  context.stroke();
 
-  //good practice to save then restore
+  //good practice
   context.restore();
+};
 
-  //draw small radius lines to illustrate radius
-  // context.beginPath();
-  // context.moveTo(0, 0);
-  // context.lineTo(x, y);
-  // context.stroke();
+//FUNCTION DECLERATION
+//Draws polygon overlay
+const drawPolygon = ({ context, radius = 100, sides = 3 }) => {
+  const slice = (Math.PI * 2) / sides;
+
+  context.beginPath();
+  context.moveTo(0, -radius);
+
+  for (let i = 1; i < sides; i++) {
+    const theta = i * slice - Math.PI * 0.5;
+    context.lineTo(Math.cos(theta) * radius, Math.sin(theta) * radius);
+  }
+
+  context.closePath();
 };
 
 canvasSketch(sketch, settings);
